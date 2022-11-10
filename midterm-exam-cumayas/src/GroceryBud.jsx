3eef1d9f4ca477bb23ff8bda27cbae4f608ac50d
Jerick Cumayas <jerickroyce.cumayas.20@usjr.edu.ph>
2022-11-10 12:34:30 +0800

    added array for items, accepts Input from button click and enter key
    - fixed error showing accepted input but message was opposite
    - can accept input from button click
    - can accept input from enter key
    - clears the input field upon button click
    - clears the input field upon enter key

--- midterm-exam-cumayas/src/GroceryBud.jsx
+++ midterm-exam-cumayas/src/GroceryBud.jsx
@@ -1,60 +1,83 @@
 import React, {useState, useEffect} from 'react'
 
 const GroceryBud = () => {
-    const[error, setError] = useState(false);
+    const[counter, setCounter] = useState(0);
+    const[errorMessage, setErrorMessage] = useState(false);
+    const[successMessage, setSuccessMessage] = useState(false);
     const[product, setProduct] = useState('');
+    const[recentlyAdded, setRecentlyAdded] = useState();
+    const[listItems, setListItems] = useState([]);
 
     useEffect(() => {
-        document.addEventListener('keydown', detectKeyDown, true)
-    }, [])
-
-    const detectKeyDown = (e) => {
-        if(e.key === "Enter"){
-            if(product === ""){
-                setError(true);
-                setTimeout(() => {
-                    setError(false);
-                }, 3000)
+        const handleKeyDown = (e) => {
+            console.log(e.key);
+            if(e.key === "Enter"){
+                (document.getElementById("enter-button")).click();
             }
         }
-    }
+        document.addEventListener("keydown", handleKeyDown);
+        return (() => {
+            document.removeEventListener("keydown", handleKeyDown);
+        })
+    },[]);
 
     const handleInputChange = (e) => {
-        setProduct(e.target.value);
+        setProduct(String(e.target.value));
     }
 
-    const handleButtonClick = (e) => {
-        if(product == ''){
-            setError(true);
+    const handleButtonClick = () => {
+        if(product.trim().length == 0 && product == ''){
+            setErrorMessage(true);
+            setProduct('');
             setTimeout(() => {
-                setError(false);
+                setErrorMessage(false);
             }, 3000)
+        } else {
+            setErrorMessage(false);
+            setSuccessMessage(true);
+            setTimeout(() => {
+                setSuccessMessage(false);
+            }, 3000)
+            listItems.push({
+                id: counter,
+                item: product
+            });
+            setRecentlyAdded(product);
+            setCounter(counter+1);
+            setProduct('');
         }
     }
 
     return(
         <div className="container">
             <div className="application-box">
+                <div className="message-container">
+                    {successMessage && (<label id = "success-message">"{recentlyAdded}" has been successfully added to cart!</label>)}  
+                    {errorMessage && ( <label id = "error-message">Field is empty. Input item to text field and click add to cart.</label>)}        
+                </div>
                 <h1 id ="header">Grocery Bud</h1>
                 <div className="search-container">
                     <input 
                         type="text" 
                         placeholder="e.g. Eggs" 
                         id="item-input" 
+                        value = {product}
                         onChange = {handleInputChange}
                     />
                     <button 
                         id="enter-button"
+                        type = "submit"
                         onClick = {handleButtonClick}
                     >Add to cart</button>
                 </div>
-                <div className="error-message-container">
-                    <label id="error-message">
-                        {error && (`Field is empty. Input item to text field and add to cart.`)}
-                    </label>
-                </div>
                 <div className="cart-containter">   
-                    
+                    <ul className="item-container">
+                        {
+                            listItems.map((items,index) => {
+                                return <li key = {index}>{items.item}</li>
+                            })
+                        }
+                    </ul>
                 </div>
             </div>
         </div>
