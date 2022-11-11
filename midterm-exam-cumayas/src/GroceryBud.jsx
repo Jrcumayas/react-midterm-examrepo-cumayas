d706c3090c447638d711fcacef1e956f352e95f8
Jerick Cumayas <jerickroyce.cumayas.20@usjr.edu.ph>
2022-11-11 23:11:18 +0800

    Final

--- midterm-exam-cumayas/src/GroceryBud.jsx
+++ midterm-exam-cumayas/src/GroceryBud.jsx
@@ -3,73 +3,122 @@ import {IoMdTrash} from 'react-icons/io'
 import {BiEdit} from 'react-icons/bi'
 
 const GroceryBud = () => {
-    const[counter, setCounter] = useState(0);
-    const[errorMessage, setErrorMessage] = useState(false);
-    const[successMessage, setSuccessMessage] = useState(false);
-    const[product, setProduct] = useState('');
-    const[recentlyAdded, setRecentlyAdded] = useState();
-    const[listItems, setListItems] = useState([]);
+    //Alert Messages for user to know what operation has been done
+    const[successMessage, setSuccessMessage] = useState(false); //Success Message is for having succesfully added an item to cart
+    const[errorMessage, setErrorMessage] = useState(false); //Error Message is for empty input field
+    const[editMessage, setEditMessage] = useState(false); //Edit Message is for succesfully editing an existing item
+    const[removeMessage, setRemoveMessage] = useState(false); //Remove Message is for having succesfully removed an item from cart
+    const[clearAllMessage, setClearAllMessage] = useState(false); // Clear All for all the items removed
+    const[editActive, setEditActive] = useState(false); // Edit Active is for the application to know whether the edit button has been activated
+
+    const[product, setProduct] = useState(''); // Product or item inputted by the user | input field and editing
+    const[recentlyRemoved, setRecentlyRemoved] = useState(); //Keep track what item has been removed
+    const[recentlyAdded, setRecentlyAdded] = useState(); //Keep track what item has been added
+    const[listItems, setListItems] = useState([]); // The Array or the list of items or products
+
+    const[selected, setSelected] = useState(); // The selected element of the array
+    const[selectedIndex, setSelectedIndex] = useState(); // The selected element's index
 
     useEffect(() => {
         var storedItems = JSON.parse(localStorage.getItem('listItems'));
         if(storedItems){
             setListItems(storedItems);
-            setCounter(storedItems.length)
-        }
+        } // To check whether an existing list is present in the Local Storage. We convert string to JSON data or format
 
         const handleKeyDown = (e) => {
-            console.log(e.key);
             if(e.key === "Enter"){
                 (document.getElementById("enter-button")).click();
             }
         }
-        document.addEventListener("keydown", handleKeyDown);
+
+        const inputField = document.getElementById("item-input");
+        inputField.addEventListener("keydown", handleKeyDown);
         return (() => {
-            document.removeEventListener("keydown", handleKeyDown);
-        })
+            inputField.removeEventListener("keydown", handleKeyDown);
+        }) // An Event listener to keep track what keys have been pressed by the user. If "Enter" key has been pressed
+           // then a click has been initialized with the id "enter-button" or our Add to Cart Button
+           // in which it performs the onClick of the button
     },[]);
 
     const handleInputChange = (e) => {
         setProduct(String(e.target.value));
-    }
+    } // Any input in the field is stored inside the Product Variable
 
     const handleButtonClick = () => {
-        if(product.trim().length == 0 && product == ''){
-            setErrorMessage(true);
-            setProduct('');
-            setTimeout(() => {
-                setErrorMessage(false);
-            }, 3000)
+        if(!editActive){ //checks if the EditActive is False
+            if(product.trim().length == 0 && product == ''){
+                setErrorMessage(true);
+                setTimeout(() => {
+                    setErrorMessage(false);
+                }, 3000)
+            } else {
+                setSuccessMessage(true);
+                setTimeout(() => {
+                    setSuccessMessage(false);
+                }, 3000)
+                listItems.push({
+                    item: product
+                });
+                localStorage.setItem('listItems', JSON.stringify(listItems));
+                setRecentlyAdded(product);
+                setProduct('');
+            }
         } else {
-            setSuccessMessage(true);
-            setTimeout(() => {
-                setSuccessMessage(false);
-            }, 3000)
-            listItems.push({
-                item: product
-            });
-            localStorage.setItem('listItems', JSON.stringify(listItems));
-            setRecentlyAdded(product);
-            setCounter(counter+1);
-            setProduct('');
+            if(product.trim().length == 0 && product == ''){
+                setErrorMessage(true);
+                setTimeout(() => {
+                    setErrorMessage(false);
+                }, 3000)
+            } else {
+                setRecentlyRemoved(selected.item);
+                setEditMessage(true);
+                setTimeout(() => {
+                    setEditMessage(false);
+                }, 3000)
+                selected.item = product;
+                setListItems(listItems);
+                localStorage.setItem('listItems', JSON.stringify(listItems));
+                setEditActive(false);
+                setProduct('');
+                setSelectedIndex();
+                setRecentlyAdded(selected.item);
+            }
         }
+        
     }
 
     const handleClearButton = () => {
-        localStorage.clear();
-        setListItems([]);
-        setCounter(0);
-        listItems.length = 0;
+        if(!editActive){
+            localStorage.clear();
+            setListItems([]);
+            listItems.length = 0;
+            setClearAllMessage(true);
+            setTimeout(() => {
+                setClearAllMessage(false);
+            }, 3000)
+        } else {
+            setProduct('');
+            setSelectedIndex();
+            setEditActive(false);
+        }
     }
 
     function handleRemoveButton(selected){
-        const newList = listItems.filter((item) => item.item !== selected);
+        setRecentlyRemoved(selected);
+        setRemoveMessage(true);
+        setTimeout(() => {
+            setRemoveMessage(false);
+        }, 3000)
+        const newList = listItems.filter((items) => items.item !== selected);
         setListItems(newList);
         localStorage.setItem('listItems', JSON.stringify(newList));
     }
 
-    function handleEditButton(selected){
-
+    function handleEditButton(selected, index){
+        setEditActive(true);
+        setSelected(selected);
+        setSelectedIndex(index);
+        setProduct(selected.item);
     }
 
     return(
@@ -77,7 +126,10 @@ const GroceryBud = () => {
             <div className="application-box">
                 <div className="message-container">
                     {successMessage && (<label id = "success-message">"{recentlyAdded}" has been successfully added to cart!</label>)}  
-                    {errorMessage && ( <label id = "error-message">Field is empty. Input item to text field and click add to cart.</label>)}        
+                    {errorMessage && ( <label id = "error-message">Field is empty. Input item and hit enter or click add to cart.</label>)}        
+                    {editMessage && (<label id = "success-message">"{recentlyRemoved}" has been editted to "{recentlyAdded}"!</label>)}
+                    {removeMessage && (<label id = "success-message">"{recentlyRemoved}" has been removed from cart!</label>)}   
+                    {clearAllMessage && (<label id = "success-message">All items are removed!</label>)}   
                 </div>
                 <h1 id ="header">Grocery Bud</h1>
                 <div className="search-container">
@@ -89,21 +141,22 @@ const GroceryBud = () => {
                         onChange = {handleInputChange}
                     />
                     <button 
+                        title = {!editActive ? "Add to List" : "Edit"}
                         id="enter-button"
                         type = "submit"
                         onClick = {handleButtonClick}
-                    >Add to cart</button>
+                    >{!editActive ? "Add to Cart" : "Edit"}</button>
                 </div>
                 <div className="cart-container">   
                     <ul className="item-container">
                         {
                             listItems.map((items,index) => {
                                 return (
-                                    <div className = "product">
+                                    <div className = {(selectedIndex === index) ? ("selected-product") : ("product")}>
                                         <li key = {index} id = "product-item">{items.item}</li>
                                         <div className = "item-buttons">
-                                            <button id = "edit-button" onClick = {()=>handleEditButton(items.item)}><BiEdit size={18}/></button>
-                                            <button id = "delete-button" onClick = {()=>handleRemoveButton(items.item)}><IoMdTrash size={18}/></button>
+                                            <button title = "Edit" id = "edit-button" onClick = {()=>handleEditButton(items, index)}><BiEdit size={18}/></button>
+                                            <button title = "Remove" id = "delete-button" onClick = {()=>handleRemoveButton(items.item)}><IoMdTrash size={18}/></button>
                                         </div>
                                     </div>
                                 )
@@ -111,7 +164,7 @@ const GroceryBud = () => {
                         }
                     </ul>
                 </div>
-                <button id="clear-all-items" onClick = {handleClearButton}>Clear All</button>
+                <button title = {!editActive? "Clear All": "Cancel"} id={!editActive? "clear-all-items": "clear-all-items-cancel"} onClick = {handleClearButton}>{!editActive? "Clear All": "Cancel"}</button>
             </div>
         </div>
     );
